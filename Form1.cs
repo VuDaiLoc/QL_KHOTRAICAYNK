bddbc9c0a5c432c59e0c8c395087193d373cc66f
VuDaiLoc <locvu2912@gmail.com>
2024-12-18 12:06:18 +0700

    Update Form1.cs

---
+++ Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLyKhoTraiCayNK
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void toolStripLabel5_Click(object sender, EventArgs e)
+        {
+
+        }
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private ToolStrip toolStrip1;
+        private ToolStripLabel toolStripLabel1;
+    }
+}
